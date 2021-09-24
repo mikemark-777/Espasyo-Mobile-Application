b26e732c3049b4551691cc3bbc78522d898ceec4
mikemark-777 <65594383+mikemark-777@users.noreply.github.com>
2021-09-24 22:40:19 +0800

    create add property UI

---
+++ app/src/main/java/com/capstone/espasyo/landlord/views/AddPropertyActivity.java
@@ -0,0 +1,47 @@
+package com.capstone.espasyo.landlord.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.telecom.Call;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.capstone.espasyo.R;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class AddPropertyActivity extends AppCompatActivity {
+
+    private Button btnAddProperty;
+    private TextInputEditText phoneNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landlord_activity_add_property);
+        btnAddProperty = findViewById(R.id.btnAddProperty);
+        phoneNumber = findViewById(R.id.text_input_landlord_phoneNumber);
+
+
+
+        btnAddProperty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Phone Number: " + phoneNumber.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    //TODO: Add input validations
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        Toast.makeText(this, "Pressing back button", Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/com/capstone/espasyo/landlord/views/DashboardFragment.java
+++ app/src/main/java/com/capstone/espasyo/landlord/views/DashboardFragment.java
@@ -1,5 +1,6 @@
 package com.capstone.espasyo.landlord.views;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,14 +9,45 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.capstone.espasyo.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class DashboardFragment extends Fragment {
+
+    private FloatingActionButton addPropertyFAB;
+    private NavController landlord_navigation;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.landlord_fragment_dashboard, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        addPropertyFAB = view.findViewById(R.id.addPropertyFAB);
+
+
+        addPropertyFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // goto add property activity
+                startActivity(new Intent(getActivity(), AddPropertyActivity.class));
+               /* Snackbar.make(view, "Add a property", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+            }
+        });
     }
 }
