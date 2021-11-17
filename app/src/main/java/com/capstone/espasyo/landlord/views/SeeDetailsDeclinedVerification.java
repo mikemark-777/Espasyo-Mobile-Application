50cca1f67f9e07585bc77c362a714a7513d8335a
mikemark-777 <65594383+mikemark-777@users.noreply.github.com>
2021-11-17 21:03:30 +0800

    update update input validations

--- app/src/main/java/com/capstone/espasyo/landlord/views/SeeDetailsDeclinedVerification.java
+++ app/src/main/java/com/capstone/espasyo/landlord/views/SeeDetailsDeclinedVerification.java
@@ -2,15 +2,32 @@ package com.capstone.espasyo.landlord.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.capstone.espasyo.R;
+import com.capstone.espasyo.models.VerificationRequest;
 
 public class SeeDetailsDeclinedVerification extends AppCompatActivity {
+
+    private VerificationRequest verificationRequest;
+
+    private TextView declinedVerificationDescription;
+
+    private String declinedVerificationReason;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landlord_activity_see_details_declined_verification);
+    }
+
+    public void initializeViews() {
+        declinedVerificationDescription = findViewById(R.id.declinedVerificationDescription);
+    }
+
+    public void getDataFromIntent(Intent intent) {
+        verificationRequest = intent.getParcelableExtra("verificationRequest");
     }
 }
