eaede8ba8c5acac2c29a236e919c151945c4a2dc
ASUS - Danica <mikemark.marcos777@gmail.com>
2022-07-07 17:37:40 +0800

    edited UI

--- app/src/main/java/com/capstone/espasyo/student/views/StudentMapFindNearestPropertiesToSMUActivity.java
+++ app/src/main/java/com/capstone/espasyo/student/views/StudentMapFindNearestPropertiesToSMUActivity.java
@@ -303,7 +303,6 @@ public class StudentMapFindNearestPropertiesToSMUActivity extends AppCompatActiv
         } else {
             return false;
         }
-
     }
 
     public void showNoInternetConnectionDialog() {
